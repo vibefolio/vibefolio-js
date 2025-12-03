09e08b85b045a6212a5c7d7a8fae9afada85b766
juuuno-coder <juuuno1116@gmail.com>
2025-12-04 01:06:01 +0900

    fix: Vercel 빌드 오류 해결 - index.js에서 모든 컴포넌트 확장자 명시

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -1,8 +1,7 @@
-export { AppHeader } from "./AppHeader";
-export { AppFooter } from "./AppFooter";
-export { AppStickyMenu } from "./AppStickyMenu";
-export { AppMainBanner } from "./AppMainBanner";
-export { AppImageCard } from "./AppImageCard";
-export { AppImageDialog } from "./AppImageDialog";
-export { AppTooltip } from "./AppTooltip";
-export {};
+export { AppHeader } from "./AppHeader.jsx";
+export { AppFooter } from "./AppFooter.jsx";
+export { AppStickyMenu } from "./AppStickyMenu.jsx";
+export { AppMainBanner } from "./AppMainBanner.jsx";
+export { AppImageCard } from "./AppImageCard.jsx";
+export { AppImageDialog } from "./AppImageDialog.jsx";
+export { AppTooltip } from "./AppTooltip.jsx"; //
